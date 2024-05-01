b9e24365e5c7325fea704dc32a95fc407bbc22cf
bigspiderk <144833020+bigspiderk@users.noreply.github.com>
2024-05-01 11:31:06 -0400

    Add files via upload

--- Encryption.java
+++ Encryption.java
@@ -8,8 +8,23 @@ import java.util.Scanner;
 public class Encryption {
 
     public static void main(String[] args) {
-        lock("test.webp");
-        unlock("test.webp.lock");
+        if (args.length > 0) {
+            String file = null;
+            if (args.length >= 2) {
+                file = args[1];
+            } else {
+                System.err.println("Please Specify File");
+                System.exit(1);
+            }
+
+            if (args[0].equalsIgnoreCase("lock")) {
+                lock(file);
+            }
+
+            if (args[0].equalsIgnoreCase("unlock")) {
+                unlock(file);
+            }
+        }
     }
 
     public static void lock(String file) {
@@ -29,7 +44,7 @@ public class Encryption {
         }
         s.close();
 
-        String binaryHash = Huffman.intToBin(hash(password));
+        String binaryHash = intToBin(hash(password));
 
         FileOutputStream out = null;
         try {
@@ -44,9 +59,11 @@ public class Encryption {
             out.write(passwordLength);
             for (int i = 0; i < passwordLength; i++) {
                 String chunck = binaryHash.substring(binaryHash.length() - 8*(i+1), binaryHash.length() - 8*i);
-                out.write(Huffman.binToInt(chunck));
+                out.write(binToInt(chunck));
             }
+
             encryptFile(in, out, password);
+            System.out.println("Successfully Locked File");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -66,9 +83,10 @@ public class Encryption {
             byte[] passwordBytes = in.readNBytes(in.read());
             StringBuilder binaryHashBuilder = new StringBuilder();
             for (byte b : passwordBytes) {
-                binaryHashBuilder.insert(0, Huffman.intToBin(b & 0xff));
+                binaryHashBuilder.insert(0, intToBin(b & 0xff));
             }
-            int passwordHash = Huffman.binToInt(binaryHashBuilder.toString());
+
+            int passwordHash = binToInt(binaryHashBuilder.toString());
 
             Scanner s = new Scanner(System.in);
             String passwordAttempt = null;
@@ -79,28 +97,25 @@ public class Encryption {
             s.close();
             String unlockedFilename = String.format("unlocked-%s", file.substring(0, file.indexOf(".lock")));
             encryptFile(in, new FileOutputStream(unlockedFilename), passwordAttempt);
+            System.out.println("Succesfully Unlocked File");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    public static void encryptFile(FileInputStream in, FileOutputStream out, String password) {
-        try {
-            int length = in.available();
-            for (int i = 0; i < length; i++) {
-                out.write(encryptByte((byte) in.read(), password.charAt(i%password.length())) &0xff);
-            }
-            in.close();
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void encryptFile(FileInputStream in, FileOutputStream out, String password) throws IOException{
+        int length = in.available();
+        for (int i = 0; i < length; i++) {
+            out.write(encryptByte((byte) in.read(), password.charAt(i%password.length())) &0xff);
         }
+        in.close();
+        out.close();
     }
 
     public static byte encryptByte(byte b, char c) {
-        String keyBits = Huffman.intToBin(c);
-        String bits = Huffman.intToBin(b &0xff);
+        String keyBits = intToBin(c);
+        String bits = intToBin(b &0xff);
         int newByte = 0;
         for (int i = bits.length()-1; i >= 0; i--) {
             if (keyBits.charAt(i) == bits.charAt(i)) {
@@ -117,8 +132,8 @@ public class Encryption {
         int currentByte = 0;
         for (int i = 0; i < text.length; i++) {
             currentByte = 0;
-            keyBits = Huffman.intToBin(key.charAt(i%key.length()));
-            bits = Huffman.intToBin(text[i] &0xff);
+            keyBits = intToBin(key.charAt(i%key.length()));
+            bits = intToBin(text[i] &0xff);
             for (int j = bits.length()-1; j >= 0; j--) {
                 if (keyBits.charAt(j) == bits.charAt(j)) {
                     currentByte += Math.pow(2, j);
@@ -134,6 +149,34 @@ public class Encryption {
         for (int i = 0; i < s.length(); i++) {
             hash = 31 * hash + s.charAt(i);
         }
-        return hash;
+        return Math.abs(hash);
+    }
+
+    public static String intToBin(int num) {
+        StringBuilder binary = new StringBuilder();
+
+        if (num == 0) {
+            binary.append(0);
+        }
+    
+        while (num > 0) {
+            int remainder = num % 2;
+            binary.append(remainder);
+            num /= 2;
+        }
+    
+        while (binary.length() % 8 != 0) {
+            binary.append('0');
+        }
+    
+        return binary.toString();
+    }
+
+    public static int binToInt(String bin) {
+        int total = 0;
+        for (int i = 0; i < bin.length(); i++) {
+            total += Math.pow(2, i) * (bin.charAt(i) % '0');
+        }
+        return total;
     }
 }
